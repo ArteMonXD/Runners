a499d7a22f11d4fe348f1b3934fd4062e20b9ee7
ArteMonXD <70392374+ArteMonXD@users.noreply.github.com>
2021-12-02 18:49:25 +0300

    Сделана моделька бегуна Начата работа над системой марафона

---
+++ Assets/Scripts/MarathonSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarathonSystem : MonoBehaviour
+{
+    public Transform relayRace;
+    private Transform localTransformRelayRace;
+    public Transform[] runners;
+    public float passDistance;
+    private int _currentRunner;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
