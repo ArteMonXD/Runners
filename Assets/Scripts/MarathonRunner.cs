7ec956d12947fa4595ba9caa4124172ee0f53a58
ArteMonXD <70392374+ArteMonXD@users.noreply.github.com>
2021-12-04 02:58:47 +0300

    Закончил работу над заданиями.

---
+++ Assets/Scripts/MarathonRunner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarathonRunner : MonoBehaviour
+{
+    public MarathonSystem marathonSystem;
+    public Vector3 targetPosition;
+    public bool run = false;
+    void Update()
+    {
+        if (run)
+        {
+            Rotate();
+            Move();
+            CheckDistance();
+        }
+    }
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * marathonSystem.speedRunners);
+    }
+    private void Rotate()
+    {
+        transform.LookAt(targetPosition);
+    }
+    private void CheckDistance()
+    {
+        float currentDistance = Vector3.Distance(transform.position, targetPosition); 
+        if (currentDistance < marathonSystem.passDistance)
+        {
+            run = false;
+            marathonSystem.SwitchRunner();
+        }
+    }
+}
--- Assets/Scripts/MarathonSystem.cs
+++ Assets/Scripts/MarathonSystem.cs
@@ -1,22 +1,93 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MarathonSystem : MonoBehaviour
 {
     public Transform relayRace;
-    private Transform localTransformRelayRace;
+    private Vector3 localPositionRelayRace;
+    private Quaternion localRotationRelayRace;
     public Transform[] runners;
+    private Vector3[] startPositions;
     public float passDistance;
+    public float speedRunners;
     private int _currentRunner;
+    private int _targetRunner;
+    public GameObject startButton;
+    public GameObject restartButton;
+    public Text parameterText;
     void Start()
     {
-        
+        localPositionRelayRace = relayRace.localPosition;
+        localRotationRelayRace = relayRace.localRotation;
+        startPositions = new Vector3[runners.Length];
+        for(int i = 0; i<runners.Length; i++)
+        {
+            startPositions[i] = runners[i].localPosition;
+        }
+        ShowMarathonState();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SwitchRunner()
     {
-        
+        if ((_currentRunner + 1) == (runners.Length - 1))
+        {
+            _currentRunner++;
+            _targetRunner = 0;
+        }
+        else if(_currentRunner == (runners.Length - 1))
+        {
+            _currentRunner = 0;
+            _targetRunner++;
+        }
+        else
+        {
+            _currentRunner++;
+            _targetRunner++;
+        }
+        ShowMarathonState();
+        PassingTheBaton();
+        SetStartRunner();
+    }
+    public void StartMarathon()
+    {
+        _targetRunner = _currentRunner+1;
+        SetStartRunner();
+        SwithButtons();
+    }
+    public void RestartMarathon()
+    {
+        for(int i = 0; i<runners.Length; i++)
+        {
+            runners[i].GetComponent<MarathonRunner>().run = false;
+            runners[i].position = startPositions[i];
+            runners[i].rotation = Quaternion.Euler(0, 0, 0);
+        }
+        _currentRunner = 0;
+        ShowMarathonState();
+        PassingTheBaton();
+        SwithButtons();
+    }
+    private void PassingTheBaton()
+    {
+        relayRace.SetParent(runners[_currentRunner]);
+        relayRace.localPosition = localPositionRelayRace;
+        relayRace.localRotation = localRotationRelayRace;
+    }
+    private void SetStartRunner()
+    {
+        MarathonRunner currentRunner = runners[_currentRunner].GetComponent<MarathonRunner>();
+        currentRunner.targetPosition = runners[_targetRunner].position;
+        currentRunner.run = true;
+    }
+    private void SwithButtons()
+    {
+        startButton.SetActive(!startButton.activeSelf);
+        restartButton.SetActive(!restartButton.activeSelf);
+    }
+    private void ShowMarathonState()
+    {
+        parameterText.text = $"Номер бегуна: {(_currentRunner + 1)}";
     }
 }
